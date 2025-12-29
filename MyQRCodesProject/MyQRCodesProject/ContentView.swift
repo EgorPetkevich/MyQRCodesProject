c1658494a15562586e2b9cd42acacbe81804da5c
Egor Petkevich <egorpetkevich01@gmail.com>
2025-12-29 16:37:15 +0300

    [+] Init Project

---
+++ MyQRCodesProject/MyQRCodesProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyQRCodesProject
+//
+//  Created by George Popkich on 29.12.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyQRCodesProject/MyQRCodesProject/MyQRCodesProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyQRCodesProjectApp.swift
+//  MyQRCodesProject
+//
+//  Created by George Popkich on 29.12.25.
+//
+
+import SwiftUI
+
+@main
+struct MyQRCodesProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
